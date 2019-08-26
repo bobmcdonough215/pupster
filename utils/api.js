d25cffd9759f28d1b8f4e740442ef3a916746919
Bob McDonough <bobmcdonough215@gmail.com>
2019-08-26 13:35:09 -0400

    added wireframe for api.js

---
+++ utils/api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { functionExpression } from "@babel/types";
+const BASEURL = "https://dog.ceo/dog-api/";
+
+export default {
+  //searchRandom: function(query) {
+    //return axios.get(BASEURL + query + APIKEY);
+
+    //searchBreed
+
+    //
+  }
+};
+
+//functionExpression
